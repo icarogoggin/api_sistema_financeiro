5d8b33f76df4f308faff2c5073fd39ef0cf03bd3
Ícaro Goggin <icaro.goggin@gmail.com>
2025-11-30 19:00:11 -0300

    feat: configure swagger module in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Financial Management API')
+    .setDescription('API for managing accounts and transactions')
+    .setVersion('1.0')
+    .addTag('finance')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
